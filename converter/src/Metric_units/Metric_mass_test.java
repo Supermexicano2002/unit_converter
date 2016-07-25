13700e80f42eb5d6124bcb39012c50b1165b50d5
Sebastian Gonzalez <supermexicano2002@gmail.com>
2016-07-24 22:14:36 -0400

    Add Metric_mass in Metric_units

---
+++ converter/src/Metric_units/Metric_mass_test.java
@@ -0,0 +1,11 @@
+package Metric_units;
+
+public class Metric_mass_test {
+
+	public static void main(String[] args){
+			Metric_mass thing = new Metric_mass();
+			System.out.println(thing.x_to_x(50, "mg", "cg"));
+			System.out.println(thing.x_to_x(500000, "kg", "g"));
+	}
+}
+	
